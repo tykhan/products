feafdac8ab04b1162ebc47d549fc2adbb850da8c
tykhan <tykhanvasyl@gmail.com>
2020-12-18 23:06:51 +0200

    Added provider

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { ItemsList } from './components/ItemsList/ItemsList';
+import { Header } from './components/Header/Header';
+import { NewItemForm } from './components/NewItemForm/NewItemForm';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <main className="main">
+        <NewItemForm />
+        <ItemsList />
+      </main>
+    </>
   );
 }
 
 export default App;
+
